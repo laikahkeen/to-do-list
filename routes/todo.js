9f5be600e4f46e66b767817d0a52fd38523f96d2
laikahkeen <laikahkeen@gmail.com>
2023-05-14 22:09:55 +0800

    create API call to controller from view

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,7 @@ const { create, read, removeTodo } = require('../controller');
 const router = express.Router();
 
 router.get('/todo/create', create);
-router.get('/todo', read);
+router.get('/todos', read);
 router.get('/todo/:id', removeTodo);
 
 module.exports = router;
